40b0e49545412e8d52b8cc3089a732a45bf6c2b0
german <german@globalzone.cl>
2021-04-07 09:09:37 -0400

    Username and password provided by /user: parameter to enforce connection with the user declared

--- unidades-systep.bat
+++ unidades-systep.bat
@@ -2,11 +2,12 @@
 
 
 NET USE * /D /Y
-NET USE Z: \\SRV1\Administracion /Y
-NET USE M: \\SRV1\Mercados_Regulacion /Y
-NET USE J: \\SRV1\Investigacion_Desarrollo /Y
-NET USE P: \\SRV1\Planificacion_Estudios /Y
-NET USE I: \\SRV1\Inteligencia_Mercado /Y
+
+NET USE Z: \\SRV1\Administracion /user:globalzone q1w2e3r4t5y6
+NET USE M: \\SRV1\Mercados_Regulacion /user:globalzone q1w2e3r4t5y6
+NET USE J: \\SRV1\Investigacion_Desarrollo /user:globalzone q1w2e3r4t5y6
+NET USE P: \\SRV1\Planificacion_Estudios /user:globalzone q1w2e3r4t5y6
+NET USE I: \\SRV1\Inteligencia_Mercado /user:globalzone q1w2e3r4t5y6
 
 @pause
 
